6826e48957127e67c4a52901cf99cefdc88bc78a
devil-dev-game <huandbne@gmail.com>
2025-01-15 14:50:27 +0700

    đã chuyển tab được nha

--- sachnoi/urls.py
+++ sachnoi/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
-    path('', views.home),
+    # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  
+    path('index.html', views.home, name='home'),  
+    path('discover.html', views.discover, name='discover'),  
+    path('author.html', views.author, name='author'),  
+    path('category.html', views.category, name='category'),  
 ]
