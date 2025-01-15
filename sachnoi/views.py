6072496ffc2f4561991aae7254b74035b1e0dae6
devil-dev-game <huandbne@gmail.com>
2025-01-15 14:50:27 +0700

    đã chuyển tab được nha

--- sachnoi/views.py
+++ sachnoi/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 
 def home(request):
     return render(request,'app/index.html')
+def author(request):
+    return render(request,'app/author.html')
+def category(request):
+    return render(
+        request,'app/category.html'
+    )
+def discover(request):
+    return render(request,'app/discover.html')
