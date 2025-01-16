93992fea75b85d8918a045942850fed3cb2068a0
Minh Tri <minhtrivo2005gg@gmail.com>
2025-01-16 10:12:02 +0700

    goi db , update models.py , update admin

--- sachnoi/urls.py
+++ sachnoi/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),  
     path('index.html', views.home, name='home'),  
     path('discover.html', views.discover, name='discover'),  
     path('author.html', views.author, name='author'),  
     path('category.html', views.category, name='category'),  
+    path('testapi/',views.testapi , name='testapi')
 ]
